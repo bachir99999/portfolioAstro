9e953352cec4f1cce12934fa3c53bcbd7917e03e
Youssef B <bachir.benmalek@gmail.com>
2025-04-11 15:20:50 +0200

    build ci cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://bachir99999.github.io',
+  base:'portfolioAstro',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
